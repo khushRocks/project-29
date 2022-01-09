53607b7ffe15ae7da553145edb1e9976e48cd241
khushRocks <90702400+khushRocks@users.noreply.github.com>
2022-01-09 19:06:21 +0530

    Add files via upload

---
+++ js/SlingShot.js
@@ -0,0 +1,38 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling = Constraint.create(options);
+        this.pointB=pointB;
+        World.add(world, this.sling);
+
+        this.rock = loadImage("rock.png");
+
+    }
+
+    fly(){
+        this.sling.bodyA =null;
+    }
+
+    display(){
+        // image(this.rock,200,20);
+        
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            
+            strokeWeight(4);
+           // write stroke() to set the color to red
+           stroke(64,224,208)
+        
+           // Write line() to draw a line from pointA to pointB
+           line(pointA.x,pointA.y, pointB.x,pointB.y);
+
+        }
+    }
+    
+}
